22c8d0af2861dff5e589e8d8ff641be0796f3da9
izamylovdev <izmaylovm@techmedia4u.com>
2021-04-04 19:32:06 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+
+const str = "string";
+
+str.slice(0, 2).toLowerCase();
 
 ReactDOM.render(
   <React.StrictMode>
